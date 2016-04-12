f6e21086f2aea73cc513636a0cbf47acafc53824
Tupeng <tupeng@gengee.cn>
2016-04-12 15:14:23 +0800

    init background

---
+++ background/src/main/java/com/tp/wb/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.tp.wb.controller;
+
+/**
+ * Created by Tupeng on 2016/4/12.
+ */
+public class IndexController {
+}
---
+++ background/src/main/java/com/tp/wb/service/IndexService.java
@@ -0,0 +1,7 @@
+package com.tp.wb.service;
+
+/**
+ * Created by Tupeng on 2016/4/12.
+ */
+public class IndexService {
+}
