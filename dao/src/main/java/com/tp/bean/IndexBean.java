b2d9459cf232f3d2738126adac7b75d8e4214bef
Tupeng <tupeng@gengee.cn>
2016-04-12 15:17:43 +0800

    init server, dao

---
+++ dao/src/main/java/com/tp/bean/IndexBean.java
@@ -0,0 +1,7 @@
+package com.tp.bean;
+
+/**
+ * Created by Tupeng on 2016/4/12.
+ */
+public class IndexBean {
+}
---
+++ server/src/test/java/MyTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Tupeng on 2016/4/12.
+ */
+public class MyTest {
+}
